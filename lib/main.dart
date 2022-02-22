26444438bfefec67cb26fe5eb6d6e57393b0a9b9
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-02-22 04:45:59 +0300

    Project is initialized.

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(primarySwatch: Colors.blue,),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Shop'),
+        ),
+        body: Center(
+            child: Text('Home page'),
+            ),
+    );
+  }
+}
