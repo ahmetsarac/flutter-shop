cdec28209f7b9286e3ffab769b1602e65d76dedf
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-02-25 01:14:39 +0300

    Cart items are made dismissible.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
@@ -9,6 +13,7 @@ class CartItem extends StatelessWidget {
   const CartItem(
       {Key? key,
       required this.id,
+      required this.productId,
       required this.price,
       required this.quantity,
       required this.title})
@@ -16,19 +21,37 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-              child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: FittedBox(child: Text('\$$price')),
-          )),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        padding: EdgeInsets.only(right: 20.0),
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40.0,
+        ),
+        alignment: Alignment.centerRight,
+        margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+                child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: FittedBox(child: Text('\$$price')),
+            )),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
+          ),
         ),
       ),
     );
