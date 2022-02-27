b59983f142ac5c99d02b50dbce03d76731174e0f
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-02-27 05:42:36 +0300

    Loading spinner is added.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -53,9 +53,9 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     final url = Uri.parse(Constants.url);
-    http
+    return http
         .post(url,
             body: json.encode({
               'title': product.title,
